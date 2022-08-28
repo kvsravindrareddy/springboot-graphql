d38f05f8d2ad05a6650f0885e4c2abd5a84f4c1f
kvsravindrareddy <kvsravindrareddy@gmail.com>
2022-08-27 20:00:17 -0700

    initial graphql commit

---
+++ src/main/java/com/veera/model/Book.java
@@ -0,0 +1,56 @@
+package com.veera.model;
+
+public class Book {
+    private Integer id;
+    private String title;
+    private Integer pages;
+    private Rating rating;
+    private Author author;
+
+    public Book() {
+
+    }
+
+    public Book(Integer id, String title, Integer pages, Rating rating, Author author) {
+        this.id = id;
+        this.title = title;
+        this.pages = pages;
+        this.rating = rating;
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
